be0cc287ef2f20eb4b807e7851eb58ec7861b50e
rafael-marquez <rafael-marquez@rafael-marquez-ThinkPad-X13-Gen-1>
2026-03-05 10:57:38 -0600

    Estructura inicial Practica 1

---
+++ src/main/java/mx/ipn/escom/practica1tdlc/Practica1TDLC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.ipn.escom.practica1tdlc;
+
+/**
+ *
+ * @author rafael-marquez
+ */
+public class Practica1TDLC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
